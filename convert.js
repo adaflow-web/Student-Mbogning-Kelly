5683f32a4562ef357f71db46ac6767181d9cac3c
Keljode <kellymbogning0@gmail.com>
2022-12-01 19:05:41 +0100

    Nothing added

---
+++ convert.js
@@ -0,0 +1,8 @@
+console.log("Hello! I hope you are fine");
+console.log("Please help me to convert 18.5 celsius to farenheit.")
+function tempConvert(celsius){
+    var fahr = (celsius * 9/5) + 32;
+    message = celsius.toString() + " degrees celsius are " + fahr.toString() + " degrees farenheit";
+    return message;
+}
+console.log(tempConvert(18.5));
