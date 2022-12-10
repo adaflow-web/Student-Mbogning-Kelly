38b4a940169a2770f7c960e46f4e2bc573f8d28f
Keljode <kellymbogning0@gmail.com>
2022-12-10 12:03:04 +0100

    AddING Thank you!

--- multiConverter.js
+++ multiConverter.js
@@ -1,28 +1,42 @@
 // celsius, chf, fcfa
 
-var userInput = window.prompt("Which conversion ? (CelsiusToFarenheit / chfToFcfa / fcfaToChf)")
+var question = window.prompt("Do you want to convert a value ? (yes or no)")
 
-if (userInput == CelsiustoFarenheit){
-    console.log(CelsiusConverter(userNumber))
+if (question == "yes"){
+
+    var userInput = window.prompt("Which conversion ? (CelsiusToFarenheit / chfToFcfa / fcfaToChf)");
+
+    if (userInput == "CelsiustoFarenheit"){
+        console.log(CelsiusConverter());
+    }
+    
+    else if ( userInput == "chfToFcfa"){
+        console.log(chfConverter());
+    }
+    
+    else if (userInput == "fcfaToChf"){
+        console.log(fcfaConverter());
+    }
+
 }
 
-else if ( userInput == chfToFcfa){
-    console.log(chfConverter(userNumber))
+else{
+    console.log("Thank you!! See you next time!!");
 }
 
-else if (userInput == fcfaToChf){
-    console.log(fcfaConverter(userNumber))
-}
+
+
+
 
 function CelsiusConverter(Celsius){
-   var userQuestion = window.prompt("Which value do you want to convert ?")
-   try{
-   userNumber = Number(userQuestion);
-}
-   catch{
-    console.log("Please enter a number");
+   var userQuestion = Number(window.prompt("Which value do you want to convert ?"))
+//    try{
+//    var userNumber = Number(userQuestion);
+// }
+//    catch{
+//     console.log("Please enter a number");
 
-   }
+//    }
 var fahr = (Celsius * 9/5) + 32;
 var message = "The result is " + fahr.toString() + " Farenheint!";
 return message;
@@ -31,31 +45,33 @@ return message;
 }
 
 function chfConverter(chf){
-    var userQuestion = window.prompt("Which value do you want to convert ?")
-    try{
-    userNumber = Number(userQuestion);
- }
-    catch{
-     console.log("Please enter a number");
+    var userQuestion = Number(window.prompt("Which value do you want to convert ?"))
+//     try{
+//     var userNumber = Number(userQuestion);
+//  }
+//     catch{
+//      console.log("Please enter a number");
  
-    }
+//     }
  var fcfa = chf * 650;
  var message = "The result is " + fcfa.toString() + " FCFA!";
  return message;
 
 }
 
-function fcfaConverter(){
-    var userQuestion = window.prompt("Which value do you want to convert ?")
-    try{
-    userNumber = Number(userQuestion);
- }
-    catch{
-     console.log("Please enter a number");
+function fcfaConverter(fcfa){
+    var userQuestion = Number(window.prompt("Which value do you want to convert ?"))
+//     try{
+//     var userNumber = Number(userQuestion);
+//  }
+//     catch{
+//      console.log("Please enter a number");
  
-    }
+//     }
  var chf = fcfa / 650;
  var message = "The result is " + chf.toString() + " CHF!";
  return message;
 
 }
+
+console.log("Thank you!!")
