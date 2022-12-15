f06f64ed4cf67f6f1aa1640b2d90436be9be42e3
Keljode <kellymbogning0@gmail.com>
2022-12-15 21:42:03 +0100

    Notapp

--- Diving_into_python/address_book.py
+++ Diving_into_python/address_book.py
@@ -1,8 +1,20 @@
-text_file = open ("Diving_into_python/address_book.txt")
-# content = text_file.read()
-for line in text_file:
-    print(line)
+# text_file = open ("Diving_into_python/address_book.txt")
+# # content = text_file.read()
+# for line in text_file:
+#     print(line)
 
-# print(content)
+# # print(content)
 
-text_file.close()
+# text_file.close()
+
+file = open("text2.txt", "w")
+file.write("Hey")
+file.close()
+
+file1 = open("kk.txt", "w")
+file1.write("Hello")
+file1.close()
+
+file2 = open("kk.txt", "a")
+file2.write("\nHow are you?")
+file2.close()
---
+++ Diving_into_python/taking_app.py
@@ -0,0 +1,80 @@
+# Create a note
+def write_note(text):
+    file = open("noteapp.txt", "a")
+    file.write("----\n")
+    file.write(text + "\n")
+    file.close()
+
+# Search through notes
+def search(text):
+    file = open("noteapp.txt")
+    content = file.read()
+    file.close()
+    result = ""
+    notes = content.split("----")
+
+    for note in notes:
+        if note.find(text) != -1:
+            result += "\n----" + note
+
+    if result == "":
+        print("Nothing found!")
+    else:
+        print(result)
+
+# Display menu
+print("What do you want to do?")
+print("Press 1 for adding a note")
+print("Press 2 for searching your notes")
+answer = input(": ")
+
+# Execute task based on menu input
+if answer == "1":
+    print("Enter your note:")
+    note = input().strip()
+    write_note(note)
+elif answer == "2":
+    print("Enter the text to search:")
+    text = input().strip()
+    search(text)
+else:
+    print("Sorry! I didn't understand that")
+
+
+
+
+
+# file = open("text2.txt")
+# content = file.read()
+# file.close()
+
+# def notapp():
+#     print("What do you want to do ? ")
+#     user_input = input("Press 1 for adding a note or Press 2 for searching a note : ")
+#     if user_input == "1":
+#         user_note = input("Enter your note : ")
+#         file = open("text2.txt", "a")
+#         file.write(user_note)
+#         file.close()
+#     elif user_input == "2":
+#         user_input = input("Enter the text to search: ")
+#         file = open("text2.txt")
+#         file.read()
+#         file.close()
+#         result = file.find(user_input)
+#         print(result)
+#     else:
+#         print("Sorry you did not enter a number between 1 and 2")
+
+# print(notapp())
+
+# def ask_to_continue():
+#     user_answer = input("Do you want to continue ? (y or n) ")
+#     if user_answer == "y":
+#         return True
+#     else:
+#         return False
+
+# while ask_to_continue():
+#     print(notapp())
+
