a53f18cdab6c8b0f76e2e8b0ac8e5f8eed1ef5de
Keljode <kellymbogning0@gmail.com>
2023-01-19 18:12:07 +0100

    flask exercises

--- Back_End_Web_Programming/Flask/fruity.py
+++ Back_End_Web_Programming/Flask/fruity.py
@@ -9,10 +9,23 @@ def open_html_file(page_name):
     content = file.read()
     file.close()
     return content
+
+
+def get_fruits():
+    fruitsdb = open("fruitydb.txt")
+    content = fruitsdb.read()
+    fruitsdb.close()
+    fruits = content.split("\n")
+    return fruits
+
 @app.route("/")
 def home_page():
     index_html = open_html_file("index")
-    return index_html
+    fruits = get_fruits()
+    actual_values = ""
+    for fruit in fruits:
+        actual_values += "<p>" + fruit + "</p>"
+    return index_html.replace("$$FRUITS$$", actual_values)
 
 @app.route("/ab")
 def ab():
---
+++ Back_End_Web_Programming/Flask_Exercises/guest_list.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def get_pages(page):
+    file = open(page)
+    content = file.read()
+    file.close()
+    return content
+
+
+
+@app.route("/")
+def welcome_page():
+    return get_pages("page1.html")
+
+
+def get_names():
+    name = open("guestdb.txt")
+    content = name.read()
+    name.close()
+    names = content.split(" ").sort()
+    return names
+
+@app.route("/guests")
+def guest_page():
+    pagee_html = get_pages("pagee.html")
+    names = get_names()
+    actual_value = ""
+    for name in names:
+        actual_value += "<p>" + name + "</p>"
+    return pagee_html.replace("$$NAMES$$", actual_value)
+
