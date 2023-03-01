72fea32c482b37012bb65af78f2743be62e5e1df
Keljode <kellymbogning0@gmail.com>
2023-03-01 15:21:58 +0100

    finalize

--- JvaScript_and_the_web/finalExercise.js
+++ JvaScript_and_the_web/finalExercise.js
@@ -5,22 +5,39 @@ const last = document.getElementById("last")
 const clearme = document.getElementById("clearme")
 const first = document.getElementById("first")
 
+function getStorage(){
+    if (localStorage.length > 0){
+        for (let i = 0; localStorage.length > i ; i++){
+            const lineCode = document.createElement("p")
+            let value = localStorage.getItem(i);
+            lineCode.innerHTML = value;
+            lineCode.className = "nice"
+            para.appendChild(lineCode);
+        }
+    }
+}
+
 function addvalue(){
     const newParagraph = document.createElement("p")
     newParagraph.innerText = Text.value;
     newParagraph.className = "nice"
     para.appendChild(newParagraph)
+    // Text.value = "";
+
+    const element = document.getElementsByClassName("nice")
+    let noElement = element.length;
+    localStorage.setItem(noElement, Text.value)
     Text.value = "";
 }
 adding.addEventListener("click", addvalue)
 
-add.onclick = () => {
-    const user = {
-        nom: para.value
+// add.onclick = () => {
+//     const user = {
+//         nom: para.value
 
-    }
-    localStorage.setItem("nom", JSON.stringify(user))
-}
+//     }
+//     localStorage.setItem("nom", JSON.stringify(user))
+// }
 
 // adding.addEventListener("click", ("nom", Text.value))
 
@@ -30,7 +47,9 @@ function remove(){
     const removeparagraph = document.getElementsByClassName("nice")
     if (removeparagraph.length > 0){
         para.removeChild(removeparagraph[0])
-    } 
+
+        localStorage.removeItem((removeparagraph.length))
+    }
     // else{
     //     alert("There is nothing")
     // }
@@ -41,16 +60,19 @@ first.addEventListener("click",remove)
 function remove1(){
     const removeparagraph1 = document.getElementsByClassName("nice")
     if (removeparagraph1.length > 0){
-        para.removeChild(removeparagraph1[removeparagraph1.length-1])}
+        para.removeChild(removeparagraph1[removeparagraph1.length-1])
     // } else{
     //     alert("There is no text")
     // }
+
+        localStorage.removeItem((removeparagraph1.length - 1))
     
-}
+} }
 last.addEventListener("click", remove1)
 
 function clearA(){
     const removeparagraph2 = document.getElementsByClassName("nice")
+    localStorage.clear()
     while (para.firstChild){
         para.removeChild(para.firstChild)
     }
@@ -67,13 +89,13 @@ clearme.onclick = () => {
     document.location.reload(); //permet de recharger la page 
 }
 
-var pageInfo = {
-    para : para,
-    Text : Text,
-    adding : add,
-    last : last,
-    clearme : clearme,
-    first : first,
-}
-localStorage.setItem("pageInfo", pageInfo )
+// var pageInfo = {
+//     para : para,
+//     Text : Text,
+//     adding : add,
+//     last : last,
+//     clearme : clearme,
+//     first : first,
+// }
+// localStorage.setItem("pageInfo", pageInfo )
 
